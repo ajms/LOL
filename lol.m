7d1736591a79f70811204cc21900b1a690e8c495
ajms <albert.schulz@gmail.com>
2013-04-05 14:59:18 +0200

    Trying other images.

--- lol.m
+++ lol.m
@@ -1,7 +1,11 @@
 function [ cost, dcost ] = lol ( phi )
 
+%{
     RGB = imread('test_images/test2.jpg');
     u0 = double(rgb2gray(RGB))/255;
+%}
+    S = load('medical_images/images1.mat');
+    u0 = S.I/255;
     u = real(ifftn(scalen(fftn(u0),[2,2],[0,0])));
     u = u(:);
     cost = 0;
@@ -11,25 +15,25 @@ function [ cost, dcost ] = lol ( phi )
     mu2 = mean(u(phi<0));
     mu = 0;
     nu = 1;
-    mom = 3;
+    mom = 2;
 
     for i = 1:M*N
         phiv = phi(i);
         iv = u(i);
         basis = floor(phiv);
         t = phiv-basis;
-        p = [-t^3+3*t^2-3*t+1 3*t^3-6*t^2+4 -3*t^3+3*t^2+3*t+1 t^3];
-        dp = [-3*t^2+6*t-3 9*t^2-12*t -9*t^2+6*t+3 3*t^2];
+        p = 1/6*[-t^3+3*t^2-3*t+1 3*t^3-6*t^2+4 -3*t^3+3*t^2+3*t+1 t^3];
+        dp = 1/6*[-3*t^2+6*t-3 9*t^2-12*t -9*t^2+6*t+3 3*t^2];
         for k = -1:2
             if basis+k == 0
-                cost = cost + mu * p(k+2);
+                cost = cost + mu*p(k+2);
             end
             if basis+k >= 0
                 cost = cost + nu*abs(iv-mu1)^mom * p(k+2);
-                dcost(i)= nu*abs(iv-mu1)^mom * dp(k+2);
+                dcost(i) = dcost(i) + nu*abs(iv-mu1)^mom * dp(k+2);
             else
                 cost = cost + abs(iv-mu2)^mom * p(k+2);
-                dcost(i) = abs(iv-mu2)^mom * dp(k+2);
+                dcost(i) = dcost(i) + abs(iv-mu2)^mom * dp(k+2);
             end
         end
     end
--- main.m
+++ main.m
@@ -1,10 +1,9 @@
 clear all;
 close all;
-%{
+
 [FileName, PathName, FilterIndex] = uigetfile('*', 'Select image');
 filename = strcat(PathName, FileName);
 [pathstr, name, ext] = fileparts(filename);
-
 if strcmpi(ext, '.mat')
     S = load(filename);
     u0 = S.I/255;
@@ -12,9 +11,10 @@ else
     RGB = imread(filename);
     u0 = double(rgb2gray(RGB))/255;
 end
-%}
+%{
 RGB = imread('test_images/test2.jpg');
 u0 = double(rgb2gray(RGB))/255;
+%}
 [M, N] = size(u0);
 
 % parameters
@@ -40,8 +40,8 @@ umean = mean(usmooth(:));
 phi = -ones(M,N);
 if circle == 1
     [X Y] = meshgrid(1:M);
-    phip = (X-50).^2 + (Y-50).^2; 
-    phi(phip <= 30^2) = 1;
+    phip = (X-150).^2 + (Y-150).^2; 
+    phi(phip <= 50^2) = 1;
 elseif circle == 0
     phip = zeros(M,N);
     for i=umin:0.005:umean
@@ -53,14 +53,44 @@ end
 phi = init(phi);
 
 options.Method = 'lbfgs';
-options.MaxIter=1000;
-options.MaxFunEvals=1000;
+figure
+pause(2);
+subplot(2,2,1);
+hold on;
+imagesc(usmooth);
+[G H] = contour(phi, [0 0],'k');
+set(H,'LineWidth',3);
+title('Initial contour and smooth image');
+axis tight;
+colorbar();
+hold off;
 
-for i=1:40
+subplot(2,2,2);
+surf(phi);
+title('Initial levelset');
+colorbar();
+
+for i=1:20
+    fprintf('Iteration: %d\n',i);
+    if i<10
+        phi = phi/50;
+    end
     x = minFunc(@lol,phi(:),options);
+    subplot(2,2,3);
     surf(reshape(x,M,N));
+    colorbar();
     phi = -ones(M,N);
     phi(x>=0) = 1;
-    phi = init(phi)/(20-0.5*i+0.1);
+    phi = init(phi);
     pause(0.1);
 end
+title('Final levelset');
+subplot(2,2,4);
+hold on;
+imagesc(u0);
+[G H] = contour(phi, [0 0]);
+set(H,'LineWidth',3);
+title('Final contour and original image');
+axis tight;
+colorbar();
+hold off;
