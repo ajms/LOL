294b98e4b5418fa1c4ba3592335db71461d5a5f3
ajms <albert.schulz@gmail.com>
2013-05-06 16:53:56 +0200

    Removed unnecessary code.

--- lol.m
+++ lol.m
@@ -1,8 +1,8 @@
-function [ cost, dcost ] = lol ( phi, I, lambda1, lambda2, nu )
+function [ F, dF, ddF ] = lol ( phi, I, lambda1, lambda2, mu, nu )
     u = I(:);
-    cost = 0;
+    F = 0;
     [M, N] = size(phi);
-    dcost = zeros(M*N,1);
+    dF = zeros(M*N,1);
     mu1 = mean(u(phi>=0));
     mu2 = mean(u(phi<0));
     mom = 2;
@@ -15,12 +15,16 @@ function [ cost, dcost ] = lol ( phi, I, lambda1, lambda2, nu )
         p = 1/6*[-t^3+3*t^2-3*t+1 3*t^3-6*t^2+4 -3*t^3+3*t^2+3*t+1 t^3];
         dp = 1/6*[-3*t^2+6*t-3 9*t^2-12*t -9*t^2+6*t+3 3*t^2];
         for k = -1:2
+            if basis+k == 0
+                F = F + mu * p(k+2);
+                dF(i) = dF(i) + mu*dp(k+2);
+            end
             if basis+k >= 0
-                cost = cost + lambda1*abs(iv-mu1)^mom * p(k+2) + nu * p(k+2);
-                dcost(i) = dcost(i) + lambda1*abs(iv-mu1)^mom * dp(k+2) + nu * dp(k+2);
+                F = F + lambda1*abs(iv-mu1)^mom * p(k+2) + nu * p(k+2);
+                dF(i) = dF(i) + lambda1*abs(iv-mu1)^mom * dp(k+2) + nu * dp(k+2);
             else
-                cost = cost + lambda2*abs(iv-mu2)^mom * p(k+2);
-                dcost(i) = dcost(i) + lambda2*abs(iv-mu2)^mom * dp(k+2);
+                F = F + lambda2*abs(iv-mu2)^mom * p(k+2);
+                dF(i) = dF(i) + lambda2*abs(iv-mu2)^mom * dp(k+2);
             end
         end
     end
--- main.m
+++ main.m
@@ -6,61 +6,38 @@ filename = strcat(PathName, FileName);
 [pathstr, name, ext] = fileparts(filename);
 if strcmpi(ext, '.mat')
     S = load(filename);
-    u0 = S.I/255;
+    I = S.I/255;
 else
     RGB = imread(filename);
-    u0 = double(rgb2gray(RGB))/255;
+    I = double(rgb2gray(RGB))/255;
 end
-%{
-RGB = imread('test_images/test2.jpg');
-u0 = double(rgb2gray(RGB))/255;
-%}
-[M, N] = size(u0);
+
+[M, N] = size(I);
 
 % parameters
-circle = 1;
-plt = 0;
-h = 1.0;
-dt = 0.1;
-di = 0.5;
+plt = 1;
+gamma = 100;
 lambda1 = 1;
 lambda2 = 1;
-mu = 0;%0.01*255^2;
+mu = 0;
 nu = 0;
 sigma = 2;
-beta = 0.01;
-doreinit = 0;
 
-usmooth = real(ifftn(scalen(fftn(u0),[sigma,sigma],[0,0])));
-umin = floor(min(usmooth(:)));
-umax = ceil(max(usmooth(:)));
-uinc = (umax-umin)/100;
-ulev = umin:uinc:umax;
-umean = mean(usmooth(:));
+Ismooth = real(ifftn(scalen(fftn(I),[sigma,sigma],[0,0])));
 
 phi = -ones(M,N);
-if circle == 1
-    [X Y] = meshgrid(1:M);
-    phip = (X-50).^2 + (Y-30).^2; 
-    phi(phip <= 20^2) = 1;
-elseif circle == 0
-    phip = zeros(M,N);
-    for i=umin:0.005:umean
-        phip = phip + PW(i,0.005,usmooth);
-    end
-    phi(phip >= 0.5) = 1;
-elseif circle == -1
-    phi = usmooth-mean(usmooth(:));
-end    
-
+%[X Y] = meshgrid(1:M);
+%phip = (X-floor(M/2)).^2 + (Y-floor(N/2)).^2; 
+%phi(phip <= 20^2) = 1;
+phi(30:60,30:60) = 1;
 phi = init(phi);
 
-options.Method = 'sd';
+%options.Method = 'sd';
 %figure
 if plt == 1
     subplot(2,2,1);
     hold on;
-    imagesc(usmooth);
+    imagesc(Ismooth);
     [G H] = contour(phi, [0 0],'k');
     set(H,'LineWidth',3);
     title('Initial contour and smooth image');
@@ -73,10 +50,9 @@ phi = phi/(max(phi(:))-min(phi(:)));
 F = ones(M*N,1);
 tic;
 for i=1:100
-    gamma = 100;
     tempF = F;
-    %x = minFunc(@lol,phi(:),options,usmooth,lambda1,lambda2,nu);
-    [F, dF] = lol(phi(:),usmooth,lambda1,lambda2,nu);
+    %x = minFunc(@lol,phi(:),options,Ismooth,lambda1,lambda2,nu);
+    [F, dF] = lol(phi(:),Ismooth,lambda1,lambda2,mu,nu);
     x = phi(:)-gamma*dF;
     if plt == 1
         subplot(2,2,2);
@@ -88,7 +64,7 @@ for i=1:100
         title('Final levelset');
         subplot(2,2,4);
         hold on;
-        imagesc(usmooth);
+        imagesc(Ismooth);
         [G H] = contour(reshape(phi,M,N), [0 0]);
         set(H,'LineWidth',3);
         title('Final contour and original image');
@@ -97,14 +73,10 @@ for i=1:100
         hold off;
     end
     phi = x;
-    %phi = -ones(M,N);
-    %phi(x>=0) = 1;
-    %phi = init(phi);
-    %phi = phi/(max(phi(:))-min(phi(:)));
     diffF = norm(F-tempF);
-    diffdF = norm(dF);
-    fprintf('Iteration: %d, difference: %f, F=%f\n ', i, diffdF, F);
-    if  diffF < 0.0001 &&  diffdF < 0.0001 && norm(dF) <0.01
+    NdF = norm(dF);
+    fprintf('Iteration: %d, difference: %f, F=%f\n ', i, NdF, F);
+    if  diffF < 0.01 &&  NdF < 0.01
        fprintf('Done after %d iterations!\n\n',i-1);
        break;
     end
@@ -112,7 +84,7 @@ end
 toc;
 if plt == 0
     hold on;
-    imagesc(usmooth);
+    imagesc(Ismooth);
     [H I] = contour(reshape(phi,M,N), [0 0]);
     axis tight;
     hold off;
