faf5b056ec45a3fe76f61cf6529118b7e04f5e2b
ajms <albert.schulz@gmail.com>
2013-05-10 11:53:56 +0200

    Added regularization.

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
-clear all;
+%clear all;
 close all;
-
+%{
 [FileName, PathName, FilterIndex] = uigetfile('*', 'Select image');
 filename = strcat(PathName, FileName);
 [pathstr, name, ext] = fileparts(filename);
@@ -11,7 +11,7 @@ else
     RGB = imread(filename);
     I = double(rgb2gray(RGB))/255;
 end
-
+%}
 [M, N] = size(I);
 
 % parameters
@@ -26,57 +26,59 @@ sigma = 2;
 Ismooth = real(ifftn(scalen(fftn(I),[sigma,sigma],[0,0])));
 
 phi = -ones(M,N);
-%[X Y] = meshgrid(1:M);
-%phip = (X-floor(M/2)).^2 + (Y-floor(N/2)).^2; 
-%phi(phip <= 20^2) = 1;
-phi(30:60,30:60) = 1;
+[X Y] = meshgrid(1:M);
+phip = (X-floor(M/2)).^2 + (Y-floor(N/2)).^2; 
+phi(phip <= 50^2) = 1;
 phi = init(phi);
 
-%options.Method = 'sd';
-%figure
+a = figure
 if plt == 1
     subplot(2,2,1);
     hold on;
-    imagesc(Ismooth);
-    [G H] = contour(phi, [0 0],'k');
-    set(H,'LineWidth',3);
+    imagesc(I);
+    contour(phi, [0 0],'Color',[1 0 0],'LineWidth',3);
     title('Initial contour and smooth image');
     axis tight;
+    colormap('gray');
     colorbar();
     hold off;
 end
-
+%options.Method = 'lbfgs';
 phi = phi/(max(phi(:))-min(phi(:)));
-F = ones(M*N,1);
+F = zeros(500,1);
+diffF = zeros(500,1);
+normdF = zeros(500,1);
+dF = 0;
 tic;
 for i=1:100
-    tempF = F;
-    %x = minFunc(@lol,phi(:),options,Ismooth,lambda1,lambda2,nu);
-    [F, dF] = lol(phi(:),Ismooth,lambda1,lambda2,mu,nu);
+    [F(i+1,1), dF] = lol(phi(:),Ismooth,lambda1,lambda2,mu,nu);
+    %dF = minFunc(@lol,phi(:),options,Ismooth,lambda1,lambda2,mu,nu);
     x = phi(:)-gamma*dF;
+    x = dF;
     if plt == 1
         subplot(2,2,2);
         surf(reshape(-gamma*dF,M,N));
+        colorbar();
         subplot(2,2,3);
         surf(reshape(x,M,N));
         colorbar();
-        pause(0.01);
         title('Final levelset');
         subplot(2,2,4);
         hold on;
         imagesc(Ismooth);
-        [G H] = contour(reshape(phi,M,N), [0 0]);
-        set(H,'LineWidth',3);
+        colormap('gray');
+        contour(reshape(phi,M,N), [0 0], 'Color', [1 0 0], 'LineWidth',2);
         title('Final contour and original image');
         axis tight;
         colorbar();
         hold off;
+        pause(0.01);
     end
+    %phi = init(x);
     phi = x;
-    diffF = norm(F-tempF);
-    NdF = norm(dF);
-    fprintf('Iteration: %d, difference: %f, F=%f\n ', i, NdF, F);
-    if  diffF < 0.01 &&  NdF < 0.01
+    diffF(i+1) = norm(F(i+1)-F(i));
+    normdF(i+1) = norm(dF);
+    if  diffF(i+1) < 1/gamma &&  normdF(i+1) < 1/gamma
        fprintf('Done after %d iterations!\n\n',i-1);
        break;
     end
@@ -85,10 +87,13 @@ toc;
 if plt == 0
     hold on;
     imagesc(Ismooth);
-    [H I] = contour(reshape(phi,M,N), [0 0]);
+    colorbar();
+    colormap('gray');
+    contour(reshape(phi,M,N), [0 0], 'Color', [1 0 0],'LineWidth',3);
     axis tight;
     hold off;
     pause(5);
     surf(reshape(x,M,N));
+    colorbar();
 end
  
